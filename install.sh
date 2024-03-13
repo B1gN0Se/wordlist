c792b989174cb527c65dca6529d5115b05b77a52
Thiago Pereira <86577632+thpereira1@users.noreply.github.com>
2024-03-12 22:45:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 apt update && apt install wget -y
 wget https://github.com/danielmiessler/SecLists/blob/master/Discovery/Web-Content/big.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/swagger.txt
-wget https://raw.githubusercontent.com/thpereira1/wordlist/main/git_wordlist.txt
